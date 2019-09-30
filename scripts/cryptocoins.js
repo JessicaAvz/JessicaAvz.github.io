26b3ed375efec233ddc41e22352160ad6cdd0bcd
Jessica Arvizu <j.arvizutr@gmail.com>
2019-09-30 15:47:03 -0500

    Update cryptocoins.js

--- scripts/cryptocoins.js
+++ scripts/cryptocoins.js
@@ -2,7 +2,7 @@ var apikey = {
     key: '919f2ec1-37e0-42d6-8e68-0625941de8ca'
 }
 
-request('GET', ' https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key)
+request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest ?CMC_PRO_API_KEY=' + apikey.key)
     .then((r1) => {
         var x1 = JSON.parse(r1.target.responseText);
         console.log(x1.data.quote.BTC.market_cap);
