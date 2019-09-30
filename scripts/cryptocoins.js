8e9e213a4753cb07c9dcd888bdb4de441f00722a
Jessica Arvizu <j.arvizutr@gmail.com>
2019-09-30 15:31:34 -0500

    Update cryptocoins.js

--- scripts/cryptocoins.js
+++ scripts/cryptocoins.js
@@ -5,7 +5,12 @@ var apikey = {
 request('GET', 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest?CMC_PRO_API_KEY=' + apikey.key)
     .then((r1) => {
         var x1 = JSON.parse(r1.target.responseText);
-        console.log(x1.data.quote.USD.total_market_cap);
+        console.log("Bitcoin" + x1.data.quote.BTC.total_market_cap);
+        console.log("Ethereum" + x1.data.quote.ETH.total_market_cap);
+        console.log("XRP" + x1.data.quote.XRP .total_market_cap);
+        console.log("Litecoin" + x1.data.quote.LTC .total_market_cap);
+        console.log("Bitcoin Cash" + x1.data.quote.BCH .total_market_cap);
+
     }).catch(err => {
         console.log(err);
     })
