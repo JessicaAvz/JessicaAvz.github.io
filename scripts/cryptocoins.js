176233903d6e786bca66f95551662b34d09a892f
Jessica Arvizu <j.arvizutr@gmail.com>
2019-09-30 15:38:50 -0500

    Update cryptocoins.js

--- scripts/cryptocoins.js
+++ scripts/cryptocoins.js
@@ -2,14 +2,14 @@ var apikey = {
     key: '919f2ec1-37e0-42d6-8e68-0625941de8ca'
 }
 
-request('GET', ' https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + apikey.key)
+request('GET', '  https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key)
     .then((r1) => {
         var x1 = JSON.parse(r1.target.responseText);
-        console.log("Bitcoin" + x1.data.quote.BTC .total_market_cap);
-        console.log("Ethereum" + x1.data.quote.ETH .total_market_cap);
-        console.log("XRP" + x1.data.quote.XRP .total_market_cap);
-        console.log("Litecoin" + x1.data.quote.LTC .total_market_cap);
-        console.log("Bitcoin Cash" + x1.data.quote.BCH .total_market_cap);
+        console.log("Bitcoin" + x1.data.quote.BTC.total_market_cap);
+        console.log("Ethereum" + x1.data.quote.ETH.total_market_cap);
+        console.log("XRP" + x1.data.quote.XRP.total_market_cap);
+        console.log("Litecoin" + x1.data.quote.LTC.total_market_cap);
+        console.log("Bitcoin Cash" + x1.data.quote.BCH.total_market_cap);
 
     }).catch(err => {
         console.log(err);
