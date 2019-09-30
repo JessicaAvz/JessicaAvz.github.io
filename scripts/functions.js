d8f9bbc8f9ec4e2cb604bd2ffeedba650b1802b5
Jessica Arvizu <j.arvizutr@gmail.com>
2019-09-30 18:29:18 -0500

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,13 +4,7 @@ var apikey = {
     key: '919f2ec1-37e0-42d6-8e68-0625941de8ca'
 }
 
-var method = {
-    method: 'GET'
-}
-
-var url = {
-    url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY='
-}
+var method = 'GET';
 
 $(document).ready(function () {
     $('.dropdown-menu a').click(function () {
@@ -18,30 +12,12 @@ $(document).ready(function () {
         $('#selectedOption').text(cryptoValue)
         console.log(cryptoValue);
 
-        var elementsArray = x1.data.filter(function (element) {
-            return element.name == cryptoValue;
-        })
-
-        console.log(elementsArray);
-        console.log(cryptoValue + " Market cap: " + elementsArray[0].quote.USD.market_cap);
-        console.log(cryptoValue + " Price: " + elementsArray[0].quote.USD.price);
-
-        var market_cap = elementsArray[0].quote.USD.market_cap;
-        var price = elementsArray[0].quote.USD.price;
-
-        var market_cap_currency = '$' + market_cap.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-        var price_currency = '$' + price.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-
-
-        $('#cryptoCoin').text(cryptoValue);
-        $('#cryptoCoinMarketCap').text(market_cap_currency);
-        $('#cryptoCoinPrice').text(price_currency);
+        doCalculus();
     });
 
     $('#refreshManually').click(function () {
-        request();
+        doCalculus();
     })
-
 
     $('#setNewMinutesButton').click(function () {
         var newMinutes = $('#newMinutesInput').val()
@@ -49,7 +25,7 @@ $(document).ready(function () {
     });
 });
 
-request(method, url + apikey.key)
+request(method, 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key)
     .then((r1) => {
         x1 = JSON.parse(r1.target.responseText);
     }).catch(err => {
@@ -64,4 +40,20 @@ function request(method, url) {
         xhr.onerror = reject;
         xhr.send();
     });
+}
+
+function doCalculus() {
+    var elementsArray = x1.data.filter(function (element) {
+        return element.name == cryptoValue;
+    })
+
+    var market_cap = elementsArray[0].quote.USD.market_cap;
+    var price = elementsArray[0].quote.USD.price;
+
+    var market_cap_currency = '$' + market_cap.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    var price_currency = '$' + price.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
+    $('#cryptoCoin').text(cryptoValue);
+    $('#cryptoCoinMarketCap').text(market_cap_currency);
+    $('#cryptoCoinPrice').text(price_currency);
 }
