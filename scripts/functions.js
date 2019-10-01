f5c2df363f1fb91b8e4d504e857d4dfa7847c221
Jessica Arvizu <j.arvizutr@gmail.com>
2019-09-30 21:40:44 -0500

    kjhkjhkj

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,5 +1,5 @@
 var x1;
-var cryptoValue;
+var cryptoValue = 'Bitcoin';
 var elementsArray;
 var method = 'GET'
 var apikey = {
@@ -7,15 +7,15 @@ var apikey = {
 }
 
 $(document).ready(function () {
-    $('.dropdown-menu a').click(function () {
-        cryptoValue = $(this).text();
-        $('#selectedOption').text(cryptoValue)
-        doCalculus();
+    $('#itemDropdown').change(function () {
+        cryptoValue = this.value;
+        console.log(cryptoValue);
+        //$('#selectedOption').text(cryptoValue)
+        updateData();
     });
 
     $('#refreshManually').click(function (e) {
-        e.preventDefault();
-        updateManually();
+        updateData();
     })
 
     $('#setNewMinutesButton').click(function () {
@@ -24,20 +24,23 @@ $(document).ready(function () {
     });
 });
 
-function updateManually() {
+function updateData() {
+    console.log('daniel es feo y popo ' + cryptoValue)
     $.ajax({
         type: "GET",
-        url: 'https://api.coinmarketcap.com/v1/ticker/',
-        data: {
-            name: cryptoValue,
-        },
+        url: `https://api.coinmarketcap.com/v1/ticker/${cryptoValue}`,
+        /*data: {
+            symbol: cryptoValue,
+        },*/
         success: function (result) {
+            console.log(result)
             var ajax_marketcap = result[0].market_cap_usd;
             var ajax_price = result[0].price_usd;
 
             var market_cap_currency = '$' + (+ajax_marketcap).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
             var price_currency = '$' + (+ajax_price).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 
+            $('#cryptoCoin').text(cryptoValue);
             $('#cryptoCoinMarketCap').text(market_cap_currency);
             $('#cryptoCoinPrice').text(price_currency);
         },
@@ -47,6 +50,10 @@ function updateManually() {
     });
 }
 
+updateData();
+
+
+/*
 function doCalculus() {
     elementsArray = x1.data.filter(function (element) {
         return element.name == cryptoValue;
@@ -78,4 +85,4 @@ function request(method, url) {
         xhr.onerror = reject;
         xhr.send();
     });
-}
+}*/
